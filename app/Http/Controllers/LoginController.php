f98c3ed50aef6909be4d0aa857a16ac6931af189
alifma <alifmaulana26@gmail.com>
2021-04-07 21:23:19 +0700

    Generate Login Token

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,11 +2,51 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class LoginController extends Controller
 {
-    public function index() {
-        echo "<h1>Halaman Login</h1>";
+    public function register(Request $request) {
+        $this -> validate($request, [
+            'email' => 'required|unique:users|email',
+            'password' => 'required|min:8'
+        ]);
+        $userData = [
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+            'level' => 'admin',
+            'api_token' => '',
+            'status' => 1
+        ];
+
+        $user = User::create($userData);
+        return response()->json($user, Response::HTTP_OK);
+    }
+
+    public function login(Request $request) {
+        $email = $request->input('email');
+        $user = User::where('email',$email)->first();
+        // Check Password
+        if (Hash::check($request->input('password'), $user->password)) {
+            $token = Str::random(40);
+            $user->update([
+                'api_token' => $token
+            ]);
+            $data = [
+                'status' => 200,
+                'msg'    => "Login Success",
+                'api_token'  => $token,
+            ];
+            return response()->json($data, Response::HTTP_OK);
+        }
+        $data = [
+            'status' => 400,
+            'msg'    => "Wrong Username Or Password",
+        ];
+        return response()->json($data, Response::HTTP_UNAUTHORIZED);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,10 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
-$router->get('login', ['uses'=> 'LoginController@index']);
-$router->group(['prefix' => 'api','middleware' => 'user'], function () use ($router) {
+$router->post('api/login', ['uses'=> 'LoginController@login']);
+$router->post('api/register', ['uses'=> 'LoginController@register']);
+
+$router->group(['prefix' => 'api','middleware' => 'auth'], function () use ($router) {
     // Category Routes
     $router->get('/category', ['uses'=> 'CategoryController@index']);
     $router->get('/category/{id}', ['uses'=> 'CategoryController@show']);
